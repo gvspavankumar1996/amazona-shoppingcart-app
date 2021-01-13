27849a8fae7beefa66c3c83d670a92003953bd10
gvspavankumar1996 <gvspavankumar1996@gmail.com>
2021-01-13 22:16:12 +0530

    git push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from './screens/CartScreen';
 import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         </div>
         </header>
         <main>
+        <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
